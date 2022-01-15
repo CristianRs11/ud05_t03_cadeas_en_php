b218fcb32c6aab37fcb754bbfb195e87cbda58a0
CristianRs11 <crodriguescalvo@gmail.com>
2022-01-15 22:08:25 +0100

    Construción de expresións en PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Construción de expresións en PHP</h3>
+        <?php
+            $primeira="Primeira liña de texto";
+            $segunda="Segunda liña de texto";
+            $terceira="Terceira liña de texto";
+            $resultado=$primeira."<br/>".$segunda."<br/>".$terceira;
+            echo $resultado;
+            echo "<br/> O número de caracteres da cadea resualtante é: ".strlen($resultado);
+        ?>
+    </body>
+</html>
